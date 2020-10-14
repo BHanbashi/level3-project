abdfc27a2a2bfef19e4902b93b69c9361a5ccdfb
Ubuntu <ubuntu@ip-10-0-1-126.me-south-1.compute.internal>
2020-10-14 19:28:31 +0000

    payment service built and ran successfully, probably

--- payment/scripts/build.sh
+++ payment/scripts/build.sh
@@ -4,21 +4,8 @@ set -ev
 
 SCRIPT_DIR=$(dirname "$0")
 
-if [[ -z "$GROUP" ]] ; then
-    echo "Cannot find GROUP env var"
-    exit 1
-fi
+DOCKER_CMD=docker
 
-if [[ -z "$COMMIT" ]] ; then
-    echo "Cannot find COMMIT env var"
-    exit 1
-fi
-
-if [[ "$(uname)" == "Darwin" ]]; then
-    DOCKER_CMD=docker
-else
-    DOCKER_CMD="sudo docker"
-fi
 CODE_DIR=$(cd $SCRIPT_DIR/..; pwd)
 echo $CODE_DIR
 
